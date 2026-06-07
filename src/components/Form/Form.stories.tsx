ccca537684b8d94f1fd7c7967fe9add64240a6e7
CameronBibb <cameron.bibb@icloud.com>
2026-06-07 00:47:37 -0400

    Expand PersonnelFeedback form story with fieldsets, address grid, textarea, and action buttons

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -1,10 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
-import { Form } from './Form';
-import { TextInput } from '../TextInput/TextInput';
-import { Label } from '../Label/Label';
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Form } from "./Form";
+import { TextInput } from "../TextInput/TextInput";
+import { Label } from "../Label/Label";
+import Button from "../Button/Button";
+import { Textarea } from "../TextArea/TextArea";
+import { Fieldset, Select } from "@trussworks/react-uswds";
 
 const meta: Meta<typeof Form> = {
-  title: 'Templates/PersonnelFeedbackForm',
+  title: "Templates/Personnel Feedback Form",
   component: Form,
   parameters: {
     docs: {
@@ -23,37 +26,98 @@ This label component is built on the USWDS label and satisfies the following WCA
       },
     },
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
     onSubmit: {
-      description: 'Submission handler',
-    }
+      description: "Submission handler",
+    },
   },
+};
 
-} 
-
-export default meta
-type Story = StoryObj<typeof Form>
+export default meta;
+type Story = StoryObj<typeof Form>;
 
 export const PersonnelFeedback: Story = {
-    render: () => (
-      <Form onSubmit={() => {}}>
+  render: () => (
+    <Form onSubmit={() => {}}>
+      <Fieldset legend="Name" legendStyle="large">
         <div>
-          <Label htmlFor='first-name'>First Name</Label>
-          <TextInput id='first-name' name='First Name' type='text'></TextInput>
+          <Label htmlFor="first-name" requiredMarker>
+            First Name
+          </Label>
+          <TextInput
+            id="first-name"
+            name="firstName"
+            type="text"
+            required
+          ></TextInput>
         </div>
         <div>
-          <Label htmlFor='last-name'>Last Name</Label>
-          <TextInput id='last-name' name='Last Name' type='text'></TextInput>
+          <Label htmlFor="last-name" requiredMarker>
+            Last Name
+          </Label>
+          <TextInput
+            id="last-name"
+            name="lastName"
+            type="text"
+            required
+          ></TextInput>
         </div>
         <div>
-          <Label htmlFor='phone'>Phone Number</Label>
-          <TextInput id='phone-number' name='Phone Number' type='tel'></TextInput>
+          <Label htmlFor="company-agency-name">
+            Company Name or State Agency
+          </Label>
+          <TextInput
+            id="company-agency-name"
+            name="agency"
+            type="text"
+          ></TextInput>
+        </div>
+      </Fieldset>
+      <br />
+      <Fieldset legend="Contact" legendStyle="large">
+        <div>
+          <Label htmlFor="street">Street Address</Label>
+          <TextInput id="street" name="street" type="text"></TextInput>
+        </div>
+        <div className="grid-row grid-gap">
+          <div className="tablet:grid-col-6">
+            <Label htmlFor="city">City</Label>
+            <TextInput id="city" name="city" type="text"></TextInput>
+          </div>
+          <div className="tablet:grid-col-3">
+            <Label htmlFor="state">State</Label>
+            <Select id="state" name="state">
+              <option>Kentucky</option>
+            </Select>
+          </div>
+          <div className="tablet:grid-col-3">
+            <Label htmlFor="zip">Zip Code</Label>
+            <TextInput id="zip" name="zip" type="text"></TextInput>
+          </div>
         </div>
         <div>
-          <Label htmlFor='email'>Email</Label>
-          <TextInput id='email' name='Email' type='email'></TextInput>
+          <Label htmlFor="phone-number">Phone Number</Label>
+          <TextInput id="phone-number" name="phone" type="tel"></TextInput>
         </div>
-      </Form>
-    )
-}
+        <div>
+          <Label htmlFor="email" requiredMarker>
+            Email
+          </Label>
+          <TextInput id="email" name="email" type="email"></TextInput>
+        </div>
+      </Fieldset>
+      <br />
+      <Fieldset legend="Comments" legendStyle="large">
+        <div>
+          <Label htmlFor="feedback-comments" requiredMarker>
+            Comments
+          </Label>
+          <Textarea id="feedback-comments" name="comments" />
+        </div>
+      </Fieldset>
+      <Button label="submit" />
+      <Button label="cancel" variant="outline" />
+    </Form>
+  ),
+};
