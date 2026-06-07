4b6af2440b043b462e87d934c1d49525fb3067db
CameronBibb <cameron.bibb@icloud.com>
2026-06-07 13:40:35 -0400

    Add KY Personnel Cabinet theme and logo, remove Storybook scaffold examples

---
+++ .storybook/KyTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "My custom Storybook",
+  brandUrl: "https://example.com",
+  brandImage: "../src/assets/KY_Personnel_Cabinet_Logo.png",
+  brandTarget: "_self",
+});
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "storybook/manager-api";
+import KyTheme from "./KyTheme";
+
+addons.setConfig({
+  theme: KyTheme,
+});
